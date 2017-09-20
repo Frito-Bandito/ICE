732f889d025786de29a629d0e38976b94a875c94
Frito-Bandito <30681754+Frito-Bandito@users.noreply.github.com>
2017-09-20 13:44:51 -0500

    added ice

---
+++ commands/hentai/hentai.js
@@ -0,0 +1,9 @@
+const commando = require('discord.js-commando');
+
+bot.on('message', (message) => {
+    
+            if(message.content == 'Hentai') {
+                 message.channel.sendMessage('Did Somebody Say Hentai?');
+             }
+          });
+         
---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('Discord.js');
+
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+/*
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();    
+bot.registry.registerCommandsIn(__dirname + '/commands');
+*/
+
+
+bot.login('MzU5OTEyMzI0MTk2MzM1NjE2.DKOHow.lp-aIt65xhD1hfCSXorFQILPjk0');
